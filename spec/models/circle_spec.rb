e403db0c9edd6925705ad148719957f4af69bff3
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-13 18:05:19 -0500

    Fix test, replaced user_id with followee_id
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>

--- spec/models/circle_spec.rb
+++ spec/models/circle_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Circle, type: :model do
   describe "relationships" do
-    it { should belong_to :user }
+    it { should belong_to :followee }
     it { should belong_to :follower }
   end
 
   describe "validations" do
-    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :followee_id }
     it { should validate_presence_of :follower_id }
   end
 end
