12c29f0c3d5cf7a4c0d4a043aa85cfa5c3942cf8
Kris Litman <kris.d.litman@gmail.com>
2021-04-13 22:08:09 -0400

    Cleaned up syntax, will need to remove views and alter controllers
    Co-authored-by: Andrew Johnston <72912074+avjohnston@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    # require "pry"; binding.pry
-    # if @user.profile_picture.attached?
-    #   @user.profile_picture.purge
-    # end
+    if @user.profile_picture.attached?
+      @user.profile_picture.purge
+    end
     @user.profile_picture.attach(params[:profile_picture])
-    # user.profile_picture.attach(io: File.open('app/assets/images/download.jpeg'),
-    # filename: 'placeholder.jpeg', content_type: 'image/jpeg')
-    # require "pry"; binding.pry
   end
 
   def index
