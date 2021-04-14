6b68b1435aeaba3ec90d4a907789f75ece015f44
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-13 20:57:09 -0500

    Create Post model, add relationships and update User relationships, all tests pass.
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 
   has_many :following_users, foreign_key: :followee_id, class_name: 'Circle'
   has_many :followers, through: :following_users
+
+  has_many :posts
 end
