3f83dbd94145ecdf59f65d096c04b255da3faa34
Andrew Johnston <av.johnst@gmail.com>
2021-04-21 17:58:28 -0600

    Change zipcode endpoint to the new versioned endpoint

--- app/services/zipcode_service.rb
+++ app/services/zipcode_service.rb
@@ -1,13 +1,13 @@
 class ZipcodeService
 
   def self.get_data(zipcode1, zipcode2)
-    response = conn.get("/distance/#{zipcode1}/#{zipcode2}")
+    response = conn.get("/api/v1/distance/#{zipcode1}/#{zipcode2}")
     JSON.parse(response.body)
   end
 
   def self.get_zipcodes(zipcode, radius)
     radius_int = radius.to_i
-    response = conn.get("/radius/#{zipcode}/#{radius_int}")
+    response = conn.get("/api/v1/radius/#{zipcode}/#{radius_int}")
     JSON.parse(response.body)
   end
 
