dabd0970d9aea703f1e068bca6c8402d4265184c
Kris Litman <kris.d.litman@gmail.com>
2021-04-21 16:47:43 -0400

    Added relationship tests to gallery_spec

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Gallery, type: :model do
- describe "Validations" do 
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :name }
- end
+  describe "Validations" do 
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :name }
+  end
+  describe "Relationships" do
+    it { should have_many :photos }
+  end
 end
